86575154e45b99db002b68afc20cfd7f0f3c1ebe
vggithub7 <gaba_vishal95@yahoo.co.in>
2020-04-28 13:46:40 +0530

    Experiment4

--- scrap_and_csv.py
+++ scrap_and_csv.py
@@ -8,9 +8,9 @@ Created on Sun Apr 26 22:11:20 2020
 import praw
 import sys
 import pandas as pd
-##reddit=praw.Reddit(client_id='Nc-Weg3_ZpmjpQ',
-#                     client_secret="75dtQXd2_TDzVqIUxMtK6QNS6Fc", 
-#                     user_agent='Testing1_api')
+reddit=praw.Reddit(client_id='Nc-Weg3_ZpmjpQ',
+                     client_secret="75dtQXd2_TDzVqIUxMtK6QNS6Fc", 
+                     user_agent='Testing1_api')
 name='%s'%(sys.argv[1])
 a="apple"
 name=a
